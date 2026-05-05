51845855d8d324f65ee32d01e36012fb1155f46a
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-05-05 17:42:02 +0530

    work on create functionality

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,4 +1,12 @@
 const tablebody = document.getElementById('tablebody')
+const studentform = document.getElementById('studentform')
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const contact = document.getElementById('contact')
+const stdtable = document.getElementById('stdtable')
+
+
 
 
 
@@ -27,23 +35,60 @@ let stdArr =[
 
 ]
 
-
+let num =0;
 function templating(arr){
   let result =``
   arr.forEach((ele,i) => {
-      result +=`<tr>
+      result +=`<tr id="${ele.std_id}">
                   <td>${i+1}</td>
                   <td>${ele.fname} ${ele.lname}</td>
                   <td>${ele.email}</td>
                   <td>${ele.contact}</td>
-                  <td><i role="button" class="fa-regular fa-pen-to-square fa-2x text-success"></i></td>
-                  <td><i role="button" class="fa-solid fa-trash text-danger fa-2x"></i></td>
+                  <td><i role="button" class="fa-regular fa-pen-to-square fa-2x text-success" onclick ="OnEdit(this)"></i></td>
+                  <td><i role="button" class="fa-solid fa-trash text-danger fa-2x"  onclick ="OnRemove(this)"></i></td>
                 </tr>`
+
+                num++;
   });
 
 
   tablebody.innerHTML =result;
 }
 
+function onsubmit(ele){
+  ele.preventDefault();
+  let newtodo ={
+    fname : fname.value,
+    lname : lname.value,
+    email : email.value,
+    contact : contact.value,
+    std_id : Date.now().toString()
+  }
+
+  stdArr.push(newtodo)
+  studentform.reset()
+  num+=1;
+  let tr = document.createElement('tr')
+  tr.id = newtodo.std_id;
+  tr.innerHTML = `<td>${num}</td>
+                  <td>${newtodo.fname} ${newtodo.lname}</td>
+                  <td>${newtodo.email}</td>
+                  <td>${newtodo.contact}</td>
+                  <td><i role="button" class="fa-regular fa-pen-to-square fa-2x text-success"></i></td>
+                  <td><i role="button" class="fa-solid fa-trash text-danger fa-2x"></i></td>`
+
+stdtable.append(tr)
+
+Swal.fire({
+  title : `The New Student ${newtodo.fname}  ${newtodo.lname} is added successfully !!!`,
+  icon : 'success',
+  timer:3000
+
+})
+
+}
+
+
 
 templating(stdArr);
+studentform.addEventListener('submit',onsubmit)
